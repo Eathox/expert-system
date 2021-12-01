e1fb2da7a91562f24c239672b3de405af008c4d9
Eathox <Rendier3636@gmail.com>
2021-12-02 00:54:16 +0100

    Use macro for run_cmd instead of functions

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,36 +1,25 @@
 mod integration_tests {
     use assert_cmd::assert::*;
-    use std::ffi::OsStr;
-
     use assert_cmd::cargo::CommandCargoExt;
     use expert_system::*;
     use std::process::Command;
 
-    fn run_cmd_args<T, S>(input: T) -> Assert
-    where
-        T: IntoIterator<Item = S>,
-        S: AsRef<OsStr>,
-    {
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-        cmd.args(input).assert()
-    }
-
-    fn run_cmd() -> Assert {
-        run_cmd_args::<Vec<String>, String>(vec![])
+    macro_rules! run_cmd {
+        ( $( $x:expr ),* ) => {{
+            let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+            $(cmd.args($x);)*
+            cmd.assert()
+        }}
     }
 
     #[test]
     fn no_arguments() {
-        let output = USAGE;
-
-        run_cmd().failure().stderr(output);
+        run_cmd!().failure().stderr(USAGE);
     }
 
     #[test]
     fn two_arguments() {
         let input = ["foo", "bar"];
-        let output = USAGE;
-
-        run_cmd_args(input).failure().stderr(output);
+        run_cmd!(input).failure().stderr(USAGE);
     }
 }
