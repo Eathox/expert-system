90cfe0a6fffd5cf25336036bc2c4ccca8c3f74cf
Eathox <pgaholster@gmail.com>
2021-12-01 22:42:11 +0100

    Add test for command line argument count check

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,18 +1,36 @@
-// TODO: Add integration tests
+mod integration_tests {
+    use assert_cmd::assert::*;
+    use std::ffi::OsStr;
 
-// mod integration_tests {
-//     use assert_cmd::prelude::*;
-//     use indoc::indoc;
-//     use std::process::Command;
+    use assert_cmd::cargo::CommandCargoExt;
+    use expert_system::*;
+    use std::process::Command;
 
-//     fn compare(input: &'static str, output: &'static str) {
-//         let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-//         cmd.arg(input).assert().success().stdout(output);
-//     }
+    fn run_cmd_args<T, S>(input: T) -> Assert
+    where
+        T: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.args(input).assert()
+    }
 
-//     #[test]
-//     fn subject_example1() {
-//         let input = "tests/valid_input/example_input.txt";
-//         let output = indoc!("");
-//     }
-// }
+    fn run_cmd() -> Assert {
+        run_cmd_args::<Vec<String>, String>(vec![])
+    }
+
+    #[test]
+    fn no_arguments() {
+        let output = USAGE;
+
+        run_cmd().failure().stderr(output);
+    }
+
+    #[test]
+    fn two_arguments() {
+        let input = ["foo", "bar"];
+        let output = USAGE;
+
+        run_cmd_args(input).failure().stderr(output);
+    }
+}
