23b719d74eb2eae871ac7d7a48d446d65d9a1601
nanderstabel <nanderstabel@gmail.com>
2021-12-01 16:52:54 +0100

    Comment out integration_tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,16 +1,18 @@
-mod integration_tests {
-    use assert_cmd::prelude::*;
-    use indoc::indoc;
-    use std::process::Command;
+// TODO: Add integration tests
 
-    fn compare(input: &'static str, output: &'static str) {
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-        cmd.arg(input).assert().success().stdout(output);
-    }
+// mod integration_tests {
+//     use assert_cmd::prelude::*;
+//     use indoc::indoc;
+//     use std::process::Command;
 
-    #[test]
-    fn subject_example1() {
-        let input = "tests/valid_input/example_input.txt";
-        let output = indoc!("");
-    }
-}
+//     fn compare(input: &'static str, output: &'static str) {
+//         let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+//         cmd.arg(input).assert().success().stdout(output);
+//     }
+
+//     #[test]
+//     fn subject_example1() {
+//         let input = "tests/valid_input/example_input.txt";
+//         let output = indoc!("");
+//     }
+// }
