0401f9fd1391dd7f67761fbd466af630473b21d3
nanderstabel <nanderstabel@gmail.com>
2021-12-09 01:44:57 +0100

    Implement first struct-like enumvariants for Token enum

--- src/structs/parser.rs
+++ src/structs/parser.rs
@@ -11,118 +11,121 @@ pub struct Parenthesis(char);
 pub struct Identifier(String);
 
 pub enum Token {
-    Implicator(Implicator),
-    Operator(Operator),
-    Parenthesis(Parenthesis),
-    Identifier(Identifier), // Maybe just a char instead of String? --> A, B, C, D
+	Implicator(Implicator),
+	Operator(Operator),
+	Parenthesis(Parenthesis),
+	Identifier(Identifier), // Maybe just a char instead of String? --> A, B, C, D
 }
 
 pub trait FromToken {
-    fn get();
+	fn get(&mut self);
 }
 
 impl FromToken for Implicator {
-    Operator::get();
-    fn get() {
-        println!("Implicator");
-    }
+	fn get(&mut self) {
+		let mut operator = Operator('|');
+		operator.get();
+		println!("Implicator");
+	}
 }
 
 impl FromToken for Operator {
-    Parenthesis::get();
-    fn get() {
-        println!("Operator");
-    }
+	fn get(&mut self) {
+		let mut parenthesis = Parenthesis('(');
+		parenthesis.get();
+		println!("Operator");
+	}
 }
 
 impl FromToken for Parenthesis {
-    Identifier::get();
-    fn get() {
-        println!("Parenthesis");
-    }
+	fn get(&mut self) {
+		let mut identifier = Identifier(String::from("A"));
+		identifier.get();
+		println!("Parenthesis");
+	}
 }
 
 impl FromToken for Identifier {
-    fn get() {
-        println!("Identifier");
-    }
+	fn get(&mut self) {
+		println!("Identifier");
+	}
 }
 
 pub struct Node<'a> {
-    token: &'a Token,
-    left: Child<'a>,
-    right: Child<'a>,
+	token: &'a Token,
+	left: Child<'a>,
+	right: Child<'a>,
 }
 
 pub struct Parser {
-    tokens: Vec<Token>,
+	tokens: Vec<Token>,
 }
 
 impl<'a> Parser {
-    pub fn new() -> Self {
-        Parser { tokens: Vec::new() }
-    }
+	pub fn new() -> Self {
+		Parser { tokens: Vec::new() }
+	}
 
-    fn tokenize(&mut self) -> Result<Vec<Token>> {
-        // Dummy Error
-        File::open(&"dummy").context(format!("Could not tokenize: {}", "Line 4"))?;
+	fn tokenize(&mut self) -> Result<Vec<Token>> {
+		// Dummy Error
+		File::open(&"dummy").context(format!("Could not tokenize: {}", "Line 4"))?;
 
-        // implementation goes here
-        todo!();
-    }
+		// implementation goes here
+		todo!();
+	}
 
-    fn get_implicator<I>(&mut self, tokens: &mut Peekable<I>) -> Result<Child>
-    where
-        I: Iterator<Item = &'a Token>,
-    {
-        // Dummy Error
-        File::open(&"dummy").context(format!("Could find implicator: {}", "Line 4"))?;
+	fn get_implicator<I>(&mut self, tokens: &mut Peekable<I>) -> Result<Child>
+	where
+		I: Iterator<Item = &'a Token>,
+	{
+		// Dummy Error
+		File::open(&"dummy").context(format!("Could find implicator: {}", "Line 4"))?;
 
-        // implementation goes here
-        todo!();
-    }
+		// implementation goes here
+		todo!();
+	}
 
-    fn get_operator<I>(&mut self, tokens: &mut Peekable<I>) -> Result<Child>
-    where
-        I: Iterator<Item = &'a Token>,
-    {
-        // Dummy Error
-        File::open(&"dummy").context(format!("Could find operator: {}", "Line 4"))?;
+	fn get_operator<I>(&mut self, tokens: &mut Peekable<I>) -> Result<Child>
+	where
+		I: Iterator<Item = &'a Token>,
+	{
+		// Dummy Error
+		File::open(&"dummy").context(format!("Could find operator: {}", "Line 4"))?;
 
-        // implementation goes here
-        todo!();
-    }
+		// implementation goes here
+		todo!();
+	}
 
-    fn get_parenthesis<I>(&mut self, tokens: &mut Peekable<I>) -> Result<Child>
-    where
-        I: Iterator<Item = &'a Token>,
-    {
-        // Dummy Error
-        File::open(&"dummy").context(format!("Could find parenthesis: {}", "Line 4"))?;
+	fn get_parenthesis<I>(&mut self, tokens: &mut Peekable<I>) -> Result<Child>
+	where
+		I: Iterator<Item = &'a Token>,
+	{
+		// Dummy Error
+		File::open(&"dummy").context(format!("Could find parenthesis: {}", "Line 4"))?;
 
-        // implementation goes here
-        todo!();
-    }
+		// implementation goes here
+		todo!();
+	}
 
-    fn get_identifier<I>(&mut self, tokens: &mut Peekable<I>) -> Result<Child>
-    where
-        I: Iterator<Item = &'a Token>,
-    {
-        // Dummy Error
-        File::open(&"dummy").context(format!("Could find identifier: {}", "Line 4"))?;
+	fn get_identifier<I>(&mut self, tokens: &mut Peekable<I>) -> Result<Child>
+	where
+		I: Iterator<Item = &'a Token>,
+	{
+		// Dummy Error
+		File::open(&"dummy").context(format!("Could find identifier: {}", "Line 4"))?;
 
-        // implementation goes here
-        todo!();
-    }
+		// implementation goes here
+		todo!();
+	}
 
-    pub fn parse(&mut self) -> Result<()> {
-        let tokens = Vec::new();
-        let mut tokens = tokens.iter().peekable();
+	pub fn parse(&mut self) -> Result<()> {
+		let tokens = Vec::new();
+		let mut tokens = tokens.iter().peekable();
 
-        self.get_implicator(&mut tokens)
-            .context(format!("Could not parse: {}", "Line 4"))?;
+		self.get_implicator(&mut tokens)
+			.context(format!("Could not parse: {}", "Line 4"))?;
 
-        // implementation goes here
-        todo!();
-    }
+		// implementation goes here
+		todo!();
+	}
 }
