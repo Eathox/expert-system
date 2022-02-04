2e08a8c6177d405957febb395ef64ca9c03fc0c7
Eathox <pgaholster@gmail.com>
2022-02-04 14:28:35 +0100

    Move lib tests into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -43,3 +43,54 @@ where
         false => Ok(result),
     }
 }
+
+#[path = "../tests/common/mod.rs"]
+mod common;
+
+#[cfg(test)]
+mod read_file {
+    use super::*;
+    use anyhow::Result;
+
+    #[test]
+    fn text() -> Result<()> {
+        let input_file = common::input_file_path("read_file/text.txt");
+        let result: Vec<String> = read_file(input_file)?;
+
+        assert_eq!(result, vec!["42", "hello world", "foo bar"]);
+        Ok(())
+    }
+
+    #[test]
+    fn numbers() -> Result<()> {
+        let input_file = common::input_file_path("read_file/numbers.txt");
+        let result: Vec<i32> = read_file(input_file)?;
+
+        assert_eq!(result, vec![1, 2, 3, 4, 5]);
+        Ok(())
+    }
+
+    #[test]
+    fn error_non_exist() {
+        let input_file = common::input_file_path("read_file/non_exist.txt");
+        let result: Result<Vec<i32>, ReadFileError> = read_file(input_file);
+        assert!(matches!(
+            result,
+            Err(crate::ReadFileError::Read { source: _ })
+        ))
+    }
+
+    #[test]
+    fn error_empty() {
+        let input_file = common::input_file_path("read_file/empty.txt");
+        let result: Result<Vec<i32>, ReadFileError> = read_file(input_file);
+        assert!(matches!(result, Err(crate::ReadFileError::Empty)))
+    }
+
+    #[test]
+    fn error_parse() {
+        let input_file = common::input_file_path("read_file/foobar.txt");
+        let result: Result<Vec<i32>, ReadFileError> = read_file(input_file);
+        assert!(matches!(result, Err(crate::ReadFileError::Parse("i32"))))
+    }
+}
