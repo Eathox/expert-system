7458699822214570756362ff54b831e991395682
Eathox <pgaholster@gmail.com>
2022-03-30 17:11:25 +0200

    Add Input struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,45 @@
 extern crate expert_system;
 use expert_system::*;
 
-use anyhow::{Context, Result};
-use std::env;
+use anyhow::{anyhow, Context, Result};
+use parser::*;
+use std::{env, path::PathBuf};
+
+pub struct Input {
+    rules: Vec<String>,
+    facts: Vec<String>,
+    queries: Vec<String>,
+}
+
+impl TryFrom<PathBuf> for Input {
+    type Error = anyhow::Error;
+
+    fn try_from(file_path: PathBuf) -> Result<Self, Self::Error> {
+        let content: Vec<String> = read_file(&file_path)
+            .context(format!("failed to read input file: '{:?}'", file_path))?;
+        let lines = sanitize::sanitize_lines(&content);
+
+        let mut sections = lines.split(|s| s == "");
+        let rules = sections
+            .next()
+            .context(format!("missing rules in: {:?}", file_path))?;
+        let facts = sections
+            .next()
+            .context(format!("missing facts in: {:?}", file_path))?;
+        let queries = sections
+            .next()
+            .context(format!("missing queries in: {:?}", file_path))?;
+        if sections.next().is_some() {
+            return Err(anyhow!("too many sections in input"));
+        }
+
+        Ok(Input {
+            rules: rules.to_vec(),
+            facts: facts.to_vec(),
+            queries: queries.to_vec(),
+        })
+    }
+}
 
 fn handle_cli() -> String {
     let args: Vec<String> = env::args().collect();
@@ -17,11 +54,7 @@ fn handle_cli() -> String {
 
 fn main() -> Result<()> {
     let input_file = handle_cli();
-    let content: Vec<String> =
-        read_file(&input_file).context(format!("failed to read input file: '{}'", input_file))?;
-
-    let lines = sanitize::sanitize_lines(&content);
-    println!("{:?}", lines);
+    let input = Input::try_from(PathBuf::from(input_file))?;
 
     let mut _parser = parser::RuleParser::new();
 
