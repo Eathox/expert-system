a6f613b6d569f34e6d8f2a6a0f030c42c0123a96
Eathox <pgaholster@gmail.com>
2022-05-20 14:33:43 +0200

    WIP Profiling

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -4,6 +4,7 @@ extern crate expert_system;
 
 use bencher::Bencher;
 use expert_system::*;
+use std::fs::File;
 
 // fn bench_medium(b: &mut Bencher) {
 //     b.iter(|| {
@@ -14,9 +15,9 @@ use expert_system::*;
 //     });
 // }
 
-fn bench_permutation_iter(b: &mut Bencher) {
-    b.iter(|| TruthTable::try_from("A + B + C + D + E + F + G + H + I + J + K + L + M + N + O + P + Q + R + S + T + U + V + W + X + Y => Z").unwrap());
-}
+// fn bench_permutation_iter(b: &mut Bencher) {
+//     b.iter(|| TruthTable::try_from("A + B + C + D + E + F + G + H + I + J + K + L + M + N + O + P + Q + R + S + T + U + V + W + X + Y => Z").unwrap());
+// }
 
 // // // fn two(b: &mut Bencher) {
 // // //     b.iter(|| TruthTable::try_from("A => Z").unwrap());
@@ -43,27 +44,25 @@ fn bench_permutation_iter(b: &mut Bencher) {
 // // benchmark_group!(benches, two, three, four, five, six);
 // benchmark_main!(benches);
 
-use std::fs::File;
-
 fn main() {
     use std::time::Instant;
-    // let guard = pprof::ProfilerGuardBuilder::default()
-    //     .frequency(1000)
-    //     .blocklist(&["libc", "libgcc", "pthread", "vdso"])
-    //     .build()
-    //     .unwrap();
+    let guard = pprof::ProfilerGuardBuilder::default()
+        .frequency(1000)
+        .blocklist(&["libc", "libgcc", "pthread", "vdso", "__sigtramp"])
+        .build()
+        .unwrap();
     let now = Instant::now();
 
-    let res = TruthTable::try_from("A + B + C + D + E + F + G + H + I + J + K + L + M + N + O + P + Q + R + S + T + U + V + W + X + Y => Z");
-    // let res = TruthTable::try_from(
-    //     "A + B + C + D + E + F + G + H + I + J + K + L + M + N + O + P + Q + R => Z",
-    // );
+    // let res = TruthTable::try_from("A + B + C + D + E + F + G + H + I + J + K + L + M + N + O + P + Q + R + S + T + U + V + W + X + Y => Z");
+    let res = TruthTable::try_from(
+        "A + B + C + D + E + F + G + H + I + J + K + L + M + N + O + P + Q + R => Z",
+    );
 
     let elapsed = now.elapsed();
-    // if let Ok(report) = guard.report().build() {
-    //     let file = File::create("flamegraph.svg").unwrap();
-    //     report.flamegraph(file).unwrap();
-    // };
+    if let Ok(report) = guard.report().build() {
+        let file = File::create("flamegraph.svg").unwrap();
+        report.flamegraph(file).unwrap();
+    };
     println!("Ok? {:?}", res.is_ok());
 
     println!("Elapsed");
